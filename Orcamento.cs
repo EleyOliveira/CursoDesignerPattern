89782f4d5d9ce0cf5254fec28e17cbb7e08c9120
Eleyfeson <eleyoliveira@bol.com.br>
2023-02-27 15:25:54 -0300

    Chain of responsability

--- Orcamento.cs
+++ Orcamento.cs
@@ -8,11 +8,18 @@ namespace CursoDesignerPattern
 {
     public class Orcamento
     {
+        public double Valor { get; private set; }
+        public IList<Item> Itens { get; private set; }
+
         public Orcamento(double valor)
         {
             Valor = valor;
         }
 
-        public double Valor { get; private set; } 
+        public void AdicionaItem(Item item)
+        {
+            Itens.Add(item);
+        }
+
     }
 }
