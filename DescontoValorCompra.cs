8fb92dc9709936e385d3bd19a60f4a4cb32edd94
Eleyfeson <eleyoliveira@bol.com.br>
2023-02-28 17:12:07 -0300

    Exercicio Chain of Responsability

---
+++ DescontoValorCompra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignerPattern
+{
+    public class DescontoValorCompra : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Desconta(Orcamento orcamento)
+        {
+            if (orcamento.Valor > 500)
+            {
+                return orcamento.Valor * 0.07;
+            }
+            else
+                return Proximo.Desconta(orcamento);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,19 +3,48 @@ using CursoDesignerPattern;
 
 Console.WriteLine("Hello, World!");
 
-IImposto iss = new ISS();
-IImposto icms = new ICMS();
-IImposto ICCC = new ICCC();
+//IImposto iss = new ISS();
+//IImposto icms = new ICMS();
+//IImposto ICCC = new ICCC();
 
-Orcamento orcamento = new Orcamento(500.00);
-Console.WriteLine(iss.Calcula(orcamento));
-Console.WriteLine(icms.Calcula(orcamento));
-Console.WriteLine(ICCC.Calcula(orcamento));
+//Orcamento orcamento = new Orcamento(500.00);
+//Console.WriteLine(iss.Calcula(orcamento));
+//Console.WriteLine(icms.Calcula(orcamento));
+//Console.WriteLine(ICCC.Calcula(orcamento));
 
-orcamento = new Orcamento(2000);
-Console.WriteLine(ICCC.Calcula(orcamento));
+//orcamento = new Orcamento(2000);
+//Console.WriteLine(ICCC.Calcula(orcamento));
 
-orcamento = new Orcamento(4000);
-Console.WriteLine(ICCC.Calcula(orcamento));
+//orcamento = new Orcamento(4000);
+//Console.WriteLine(ICCC.Calcula(orcamento));
+
+Console.WriteLine("Desconto");
+Orcamento orcamento = new Orcamento(200);
+IDesconto di = new DescontoQuantidadeItem();
+IDesconto dv = new DescontoValorCompra();
+IDesconto sd = new SemDesconto();
+
+for (int i = 0;i < 10; i++)
+{
+    Item item = new Item($"Borracha {i + 1}", 5 + i);
+    orcamento.AdicionaItem(item);
+}
+
+di.Proximo = dv;
+dv.Proximo = sd;
+
+Console.WriteLine(di.Desconta(orcamento));
+
+
+
+
+
+
+
+
+
+
+
+
 
 Console.ReadKey();
