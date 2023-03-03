f73f8b5a6a8291dbef63796691ba3463dba0b854
Eleyfeson <eleyoliveira@bol.com.br>
2023-03-03 18:35:21 -0300

    DescontoPorVendaCasada

---
+++ Principal/DescontoPorVendaCasada.cs
@@ -0,0 +1,24 @@
+﻿namespace CursoDesignerPattern
+{
+    public class DescontoPorVendaCasada : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Desconta(Orcamento orcamento)
+        {
+            if (AconteceuVendaCasada(orcamento))
+            {
+                return orcamento.Valor * 0.05;
+            }
+            else
+            {
+                return Proximo.Desconta(orcamento);
+            }            
+        }
+
+        private static bool AconteceuVendaCasada(Orcamento orcamento) 
+        {
+            return orcamento.Itens.Where(i => i.Nome == "Caneta" || i.Nome == "Lapis").Count() == 2;
+        }
+    }
+}
--- Principal/Program.cs
+++ Principal/Program.cs
@@ -19,19 +19,28 @@ Console.WriteLine("Hello, World!");
 //Console.WriteLine(ICCC.Calcula(orcamento));
 
 //Console.WriteLine("Desconto");
-Orcamento orcamento = new Orcamento(510);
+Orcamento orcamento = new Orcamento(200);
 IDesconto di = new DescontoQuantidadeItem();
 IDesconto dv = new DescontoValorCompra();
+IDesconto dvc = new DescontoPorVendaCasada();
 IDesconto sd = new SemDesconto();
 
-for (int i = 0; i < 2; i++)
-{
-    Item item = new Item($"Borracha {i + 1}", 5 + i);
-    orcamento.AdicionaItem(item);
-}
+Item item = new Item("Caneta", 20);
+orcamento.AdicionaItem(item);
+item = new Item("Lapiseira", 20);
+orcamento.AdicionaItem(item);
+item = new Item("Caderno", 20);
+orcamento.AdicionaItem(item);
+item = new Item("Caderno", 20);
+orcamento.AdicionaItem(item);
+item = new Item("Caderno", 20);
+orcamento.AdicionaItem(item);
+//item = new Item("Caderno", 20);
+//orcamento.AdicionaItem(item);
 
 di.Proximo = dv;
-dv.Proximo = sd;
+dv.Proximo = dvc;
+dvc.Proximo = sd;
 
 Console.WriteLine(di.Desconta(orcamento));
 
